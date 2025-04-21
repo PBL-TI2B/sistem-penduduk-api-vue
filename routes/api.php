c292d4f89e2bbe4fc18cd266364f97b442aa8818
Wrseno <warsenosetyono@gmail.com>
2025-04-21 19:10:42 +0700

    feat: add api routes for auth and penduduk

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\PendudukController;
+
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register'])->name('register');
+    Route::post('/login', [AuthController::class, 'login'])->name('login');
+    
+    Route::middleware('auth:sanctum')->group(function(){
+        Route::apiResource('/penduduk', PendudukController::class);
+        Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    });
+});
