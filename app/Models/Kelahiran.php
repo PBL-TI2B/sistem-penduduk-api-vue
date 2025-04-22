0848a062520341018c9e07da636b6164dbb11631
SamwanElse <nicholas.anasthasius23@gmail.com>
2025-04-22 10:55:51 +0700

    add kelahiran

--- app/Models/Kelahiran.php
+++ app/Models/Kelahiran.php
@@ -20,6 +20,11 @@ class Kelahiran extends Model
         });
     }
 
+    public function penduduk()
+    {
+        return $this->belongsTo(Penduduk::class, 'penduduk_id', 'id');
+    }
+
     public function getRouteKeyName()
     {
         return 'uuid';
