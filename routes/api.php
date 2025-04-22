7819663c9b84ac4baa9fb8112086b4aa559dfaa7
AzaniFatturFadhika <affadhika287@gmail.com>
2025-04-22 10:40:23 +0700

    add jabatan

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,16 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\PendudukController;
+use App\Http\Controllers\Api\JabatanController;
 
 Route::prefix('v1')->group(function () {
     Route::post('/register', [AuthController::class, 'register'])->name('register');
     Route::post('/login', [AuthController::class, 'login'])->name('login');
-    
+
     Route::middleware('auth:sanctum')->group(function(){
         Route::apiResource('/penduduk', PendudukController::class);
+        Route::apiResource('/jabatan', JabatanController::class);
+
         Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
     });
 });
