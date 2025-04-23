640902c91dae83d51595a612d569a5f6c4467176
Wrseno <warsenosetyono@gmail.com>
2025-04-23 08:14:07 +0700

    feat: add some library

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            "ziggy-js": path.resolve("vendor/tightenco/ziggy"),
+        },
+    },
     plugins: [
         vue(),
         laravel({
