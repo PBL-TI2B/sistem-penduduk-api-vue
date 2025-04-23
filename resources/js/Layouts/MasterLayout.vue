80254f03b14fc24f1a516b5fc13fb2aafe45f5c6
Wrseno <warsenosetyono@gmail.com>
2025-04-23 08:16:33 +0700

    feat: add master layout

--- resources/js/Layouts/MasterLayout.vue
+++ resources/js/Layouts/MasterLayout.vue
@@ -1,16 +1,18 @@
 <script setup>
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import AppSidebar from "@/components/AppSidebar.vue";
+import AppSidebar from "@/components/master/AppSidebar.vue";
+import { Toaster } from "vue-sonner";
 </script>
 
 <template>
     <div class="flex">
         <SidebarProvider>
             <AppSidebar />
-            <main class="flex-1 my-8">
-                <SidebarTrigger class="md:hidden" />
+            <main class="flex-1 my-2 px-8">
+                <SidebarTrigger class="lg:hidden" />
                 <slot />
             </main>
         </SidebarProvider>
     </div>
+    <Toaster position="top-right" />
 </template>
