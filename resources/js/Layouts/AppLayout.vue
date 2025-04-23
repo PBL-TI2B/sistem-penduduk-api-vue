a438336398542227e87aaef42ddca6bd28bbe971
Wrseno <warsenosetyono@gmail.com>
2025-04-23 08:16:24 +0700

    feat: add app layout

---
+++ resources/js/Layouts/AppLayout.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { Toaster } from "vue-sonner";
+</script>
+
+<template>
+    <nav>
+        <ul class="flex space-x-4">
+            <li>
+                <a href="#" class="text-blue-500 hover:underline">Home</a>
+            </li>
+            <li>
+                <a href="#" class="text-blue-500 hover:underline">About</a>
+            </li>
+            <li>
+                <a href="#" class="text-blue-500 hover:underline">Contact</a>
+            </li>
+        </ul>
+    </nav>
+    <slot />
+    <Toaster position="top-right" />
+</template>
