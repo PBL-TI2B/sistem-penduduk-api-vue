f817bfdc15e150233b32dc603f56f1b623ffe1ff
Warseno <105684275+Wrseno@users.noreply.github.com>
2025-05-04 10:05:47 +0700

    refactor: refactor code

--- app/Http/Controllers/Api/V1/NotifikasiController.php
+++ app/Http/Controllers/Api/V1/NotifikasiController.php
@@ -54,16 +54,7 @@ class NotifikasiController extends Controller
             'message' => 'Detail Notifikasi',
             'data' => new NotifikasiResource($notifikasi),
         ]);
-    }
 
-        $notifikasi = Notifikasi::create([
-            'aksi_user_id' => $request->aksi_user_id,
-            'jenis' => $request->jenis,
-            'deskripsi' => $request->deskripsi,
-            'waktu_aksi' => $request->waktu_aksi,
-        ]);
-
-        return new ApiResource(true, 'Notifikasi Berhasil Ditambahkan', $notifikasi);
     }
 
     public function update(Request $request, Notifikasi $notifikasi)
