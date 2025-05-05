5aa5398e09d3196229164294dc00930d7a425cbf
NicholasErnesto <nicholas.ernesto23@gmail.com>
2025-05-06 06:31:14 +0700

    views pekerjaan

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,17 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\Master\PendudukController;
+use App\Http\Controllers\Web\Master\PekerjaanController;
 use App\Http\Middleware\EnsureUserIsAdmin;
 use App\Http\Middleware\EnsureTokenAuthenticated;
 
 Route::inertia('/', 'Home');
 Route::inertia('/login', 'Auth/Login');
 
-    Route::inertia('/dashboard', 'Master/Dashboard');
-    Route::resource('/penduduk', PendudukController::class);
+Route::inertia('/dashboard', 'Master/Dashboard');
+Route::resource('/penduduk', PendudukController::class);
+Route::resource('/pekerjaan', PekerjaanController::class);
+
+Route::inertia('/bansos', 'Bansos');
+Route::inertia('/infografis', 'Infografis');
+Route::inertia('/berita', 'Berita');
