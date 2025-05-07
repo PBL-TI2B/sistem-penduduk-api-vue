b6daad178ade7f50b6281750f2aa1df026a663fc
AhmadRizkiadi <rizkiahmad7396@gmail.com>
2025-05-07 10:29:19 +0700

    admin - pendidikan vue

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,18 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\Master\PendudukController;
 use App\Http\Middleware\EnsureUserIsAdmin;
 use App\Http\Middleware\EnsureTokenAuthenticated;
+use App\Http\Controllers\Web\Master\PendidikanController;
 
 Route::inertia('/', 'Home');
 Route::inertia('/login', 'Auth/Login');
 
-    Route::inertia('/dashboard', 'Master/Dashboard');
-    Route::resource('/penduduk', PendudukController::class);
+Route::inertia('/dashboard', 'Master/Dashboard');
+Route::resource('/penduduk', PendudukController::class);
+Route::resource('/pendidikan', PendidikanController::class);
+
+Route::inertia('/infografis/bansos', 'InfografisBansos');
+Route::inertia('/infografis/penduduk', 'InfografisPenduduk');
+Route::inertia('/berita', 'Berita');
+Route::inertia('/profildesa', 'ProfilDesa');
+Route::inertia('/galeri', 'Galeri');
+Route::inertia('/detailberita', 'DetailBerita');
