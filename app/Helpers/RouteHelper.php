adf2a0fb7a325fb23a0019af4502379bc5c8372f
Warseno <105684275+Wrseno@users.noreply.github.com>
2025-05-13 07:51:42 +0700

    feat: add route role permission helper

---
+++ app/Helpers/RouteHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+function RoutePermission($path, $controller, $model, $publicGet = false) {
+    Route::prefix($path)->group(function () use ($path, $controller, $model, $publicGet) {
+        if ($publicGet) {
+            Route::get('/', [$controller, 'index']);
+            Route::get('/{' . $model . '}', [$controller, 'show']);
+        } else {
+            Route::get('/', [$controller, 'index'])->middleware("auth:sanctum");
+            Route::get('/{' . $model . '}', [$controller, 'show'])->middleware("auth:sanctum");
+        }
+
+        Route::post('/', [$controller, 'store'])
+            ->middleware(["auth:sanctum", "permission:{$path}.create"]);
+
+        Route::put('/{' . $model . '}', [$controller, 'update'])
+            ->middleware(["auth:sanctum", "permission:{$path}.update"]);
+
+        Route::delete('/{' . $model . '}', [$controller, 'destroy'])
+            ->middleware(["auth:sanctum", "permission:{$path}.delete"]);
+    });
+}
+
