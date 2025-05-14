5f1533c751365a84ea219600c2305146a984f829
Warseno <105684275+Wrseno@users.noreply.github.com>
2025-05-14 07:08:00 +0700

    feat: change some resources to show id

--- app/Http/Resources/PerangkatDesaResource.php
+++ app/Http/Resources/PerangkatDesaResource.php
@@ -17,12 +17,32 @@ class PerangkatDesaResource extends JsonResource
         return [
             'id' => $this->id,
             'uuid' => $this->uuid,
-            'nama_lengkap' => $this->penduduk?->nama_lengkap,
-            'jabatan' => $this->jabatan?->jabatan,
-            'desa' => $this->desa?->nama,
-            'dusun' => $this->dusun?->nama,
-            'rw' => $this->rw?->nomor_rw,
-            'rt' => $this->rt?->nomor_rt,
+            'penduduk' =>[ 
+                'id'=> $this->penduduk?->id,
+                'nama_lengkap' => $this->penduduk?->nama_lengkap
+            ],
+            'foto'=> $this->penduduk?->foto,
+            'jabatan' => [
+            'id'=> $this->jabatan?->id,
+                'jabatan'=> $this->jabatan?->jabatan
+            ],
+            'desa' => [
+                'id'=> $this->desa?->id,
+                'nama'=>$this->desa?->nama
+            ],
+            'dusun' => [
+                'id'=> $this->dusun?->id,
+                'nama'=>$this->dusun?->nama
+            ]
+            ,
+            'rw' => [
+                'id'=> $this->rw?->id,
+                'nomor_rw'=>$this->rw?->nomor_rw
+            ],
+            'rt' => [
+                'id'=> $this->rt?->id,
+                'nomor_rt'=>$this->rt?->nomor_rt
+            ],
             'status_keaktifan' => $this->status_keaktifan,
             'periode_jabatan' => [
                 'id'=> $this->periode_jabatan?->id,
