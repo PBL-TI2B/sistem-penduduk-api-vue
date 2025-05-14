61ccd356896a0b4234dc0438f636abc49a1afe11
Warseno <105684275+Wrseno@users.noreply.github.com>
2025-05-14 07:08:42 +0700

    feat: add delete alert components

---
+++ resources/js/components/master/AlertDialog.vue
@@ -0,0 +1,46 @@
+<script setup>
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+
+const props = defineProps({
+    isOpen: Boolean,
+    title: String,
+    description: String,
+    onConfirm: Function,
+    onCancle: Function,
+});
+
+const emit = defineEmits(["update:isOpen"]);
+</script>
+
+<template>
+    <AlertDialog
+        :open="props.isOpen"
+        @openChange="emit('update:isOpen', $event)"
+    >
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>{{ props.title }}</AlertDialogTitle>
+                <AlertDialogDescription>
+                    {{ props.description }}
+                </AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogCancel @click="props.onCancle"
+                    >Batal</AlertDialogCancel
+                >
+                <AlertDialogAction @click="props.onConfirm"
+                    >Ya</AlertDialogAction
+                >
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
+</template>
