73fb7abe2df1e233591c85294d412e2956b39a9f
FaturTuru <affadhika287@gmail.com>
2025-05-18 16:50:50 +0700

    update API kategori-bantuan

--- app/Models/KategoriBantuan.php
+++ app/Models/KategoriBantuan.php
@@ -7,24 +7,28 @@ use Illuminate\Support\Str;
 
 class KategoriBantuan extends Model
 {
-    protected $table = 'kategori_bantuan';
     protected $guarded = [];
+    protected $table = 'kategori_bantuan';
+    public $incrementing = true;
+    protected $keyType = 'string';
 
     protected static function boot()
     {
         parent::boot();
         static::creating(function ($model) {
-            if (!$model->id) {
-                $model->id = Str::uuid();
+            if (!$model->uuid) {
+                $model->uuid = Str::uuid();
             }
         });
     }
 
-    public $incrementing = false;
-    protected $keyType = 'string';
-
     public function bantuan()
     {
         return $this->hasMany(Bantuan::class, 'kategori_bantuan_id', 'id');
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'uuid';
     }
 }
