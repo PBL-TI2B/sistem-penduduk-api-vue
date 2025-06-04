ebb3585e406a162b0eea3997f143afd725bdaa22
Rohimatun <rohimatunnurin13@gmail.com>
2025-06-04 20:55:27 +0700

    search & total data di Kematian

--- resources/js/Pages/Master/Kematian/Index.vue
+++ resources/js/Pages/Master/Kematian/Index.vue
@@ -3,6 +3,7 @@ import { route } from "ziggy-js";
 import { ref, onMounted, watch } from "vue";
 import { apiGet } from "@/utils/api";
 import { actionsIndex, columnsIndex } from "./utils/table";
+import { PackagePlus, SearchIcon } from "lucide-vue-next";
 
 import Button from "@/components/ui/button/Button.vue";
 import Input from "@/components/ui/input/Input.vue";
@@ -12,6 +13,7 @@ import { useErrorHandler } from "@/composables/useErrorHandler";
 
 const items = ref([]);
 const totalPages = ref(1);
+const totalData = ref(0);
 const page = ref(1);
 const perPage = ref(10);
 const isLoading = ref(false);
@@ -31,6 +33,7 @@ const fetchData = async () => {
         }));
         perPage.value = res.data.per_page;
         totalPages.value = res.data.last_page;
+        totalData.value = res.data.total;
     } catch (error) {
         useErrorHandler(error, "Gagal memuat data kematian");
     } finally {
@@ -40,10 +43,18 @@ const fetchData = async () => {
 
 onMounted(fetchData);
 watch(page, fetchData);
-watch(searchKematian, () => {
+const onSearchEnter = (e) => {
+    if (e.key === "Enter") {
+        page.value = 1;
+        fetchData();
+    }
+};
+
+const clearSearchKematian = () => {
+    searchKematian.value = "";
     page.value = 1;
     fetchData();
-});
+};
 </script>
 
 <template>
@@ -59,23 +70,41 @@ watch(searchKematian, () => {
                 ]"
             />
         </div>
-        <div class="flex flex-wrap gap-4 items-center">
-            <Button asChild>
-                <Link :href="route('kematian.create')">+ Kematian</Link>
-            </Button>
-        </div>
     </div>
 
     <!-- Search -->
-    <div class="drop-shadow-md w-full grid gap-2">
-        <div
-            class="bg-primary-foreground p-2 rounded-lg flex flex-wrap gap-2 justify-between items-center"
-        >
-            <Input
-                v-model="searchKematian"
-                placeholder="Cari data kematian berdasarkan nama atau sebab"
-                class="md:w-1/3"
-            />
+    <div class="drop-shadow-md w-full grid gap-2 mb-3">
+        <div class="flex xl:flex-row flex-col gap-4 items-center">
+            <div
+                class="flex bg-primary-foreground relative items-center p-2 rounded-lg gap-2 justify-between w-full"
+            >
+                <Input
+                    v-model="searchKematian"
+                    @keyup.enter="onSearchEnter"
+                    placeholder="Cari data kematian"
+                    class="pl-10 pr-8"
+                />
+                <span
+                    class="absolute start-2 inset-y-0 flex items-center justify-center px-2"
+                >
+                    <SearchIcon class="size-6 text-muted-foreground" />
+                </span>
+                <button
+                    v-if="searchKematian"
+                    @click="clearSearchKematian"
+                    class="absolute end-2 inset-y-0 flex items-center px-2 text-muted-foreground hover:text-primary"
+                    title="Hapus pencarian"
+                >
+                    ✕
+                </button>
+            </div>
+            <div
+                class="flex bg-primary-foreground p-2 rounded-lg gap-2 justify-between"
+            >
+                <Button asChild>
+                    <Link :href="route('kematian.create')">+ Kematian</Link>
+                </Button>
+            </div>
         </div>
 
         <!-- Data Table -->
@@ -84,6 +113,7 @@ watch(searchKematian, () => {
             :columns="columnsIndex"
             :actions="actionsIndex"
             :totalPages="totalPages"
+            :total-data="totalData"
             :page="page"
             :per-page="perPage"
             :is-loading="isLoading"
