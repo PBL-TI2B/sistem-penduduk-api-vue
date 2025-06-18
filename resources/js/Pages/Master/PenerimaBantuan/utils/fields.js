52fa50bac9a7f51910cc9c74c1cc0f7a82ba53aa
AzaniFatturFadhika <affadhika287@gmail.com>
2025-06-19 00:54:01 +0700

    modify penerima_bantuan-still error

--- resources/js/Pages/Master/PenerimaBantuan/utils/fields.js
+++ resources/js/Pages/Master/PenerimaBantuan/utils/fields.js
@@ -1,6 +1,19 @@
 export function getFields() {
     return [
-
+        {
+            name: "bantuan_id",
+            type: "hidden",
+            readonly: true,
+            disabled: false,
+            hidden: true
+        },
+        {
+            name: "kurang_mampu_id",
+            type: "hidden",
+            readonly: true,
+            disabled: false,
+            hidden: true
+        },
         {
             name: "nama_penduduk",
             label: "Nama Penduduk",
--- resources/js/Pages/Master/PenerimaBantuan/utils/form-schema.js
+++ resources/js/Pages/Master/PenerimaBantuan/utils/form-schema.js
@@ -3,6 +3,8 @@ import * as z from "zod";
 
 export const formSchemaPenerimaBantuan = toTypedSchema(
     z.object({
+        bantuan_id: z.number(),
+        kurang_mampu_id: z.number(),
         nama_penduduk: z.string(),
         nama_bantuan: z.string(),
         tanggal_penerimaan: z.date(),
